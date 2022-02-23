d5e5f9b4ca56fd06df6df0438bad0125177a15cd
bahrilio <mbahrililmi2@gmail.com>
2022-02-23 20:25:54 +0700

    belajar layouting

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    {{-- head --}}
+    @include('partials.head');
+
+    <body class="sb-nav-fixed">
+
+        {{-- navbar --}}
+        @include('partials.navbar');
+
+        <div id="layoutSidenav">
+
+            {{-- sidebar --}}
+           @include('partials.sidebar');
+
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        {{-- Judul Content --}}
+                        <h1 class="mt-1">@yield('title')</h1>
+                            {{-- Isi Content --}}
+                            @yield('container')
+                    </div>
+                </main>
+
+                {{-- footer --}}
+                @include('partials.footer');
+
+            </div>
+        </div>
+
+        {{-- foot --}}
+        @include('partials.foot');
+        
+    </body>
+</html>
